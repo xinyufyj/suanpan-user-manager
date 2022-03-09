8cb2a4f656ac337f4022ce3a444e58cebe6d70c8
magic-zhu <18817313320@163.com>
2022-03-09 21:15:41 +0800

    fix

--- src/components/UserCreate.vue
+++ src/components/UserCreate.vue
@@ -1,17 +1,18 @@
 <template>
-  <a-modal 
-    v-model:visible="modalVisible" 
+  <a-modal
+    v-model:visible="modalVisible"
     title="新增"
     :maskClosable="false"
     :afterClose="afterCloseHandler"
-    :footer="null">
-      <a-form
-        ref="model"
-        :model="modelState"
-        :rules="rules"
-        :label-col="labelCol"
-        :wrapper-col="wrapperCol"
-      >
+    :footer="null"
+  >
+    <a-form
+      ref="model"
+      :model="modelState"
+      :rules="rules"
+      :label-col="labelCol"
+      :wrapper-col="wrapperCol"
+    >
       <a-form-item label="用户名" name="username">
         <a-input v-model:value="modelState.username" />
       </a-form-item>
@@ -26,124 +27,148 @@
       </a-form-item>
       <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
         <a-button type="default" @click="cancel">取消</a-button>
-        <a-button style="margin-left: 10px" type="primary" :loading="loading" @click="createUser">确定</a-button>
+        <a-button
+          style="margin-left: 10px"
+          type="primary"
+          :loading="loading"
+          @click="createUser"
+          >确定</a-button
+        >
       </a-form-item>
     </a-form>
   </a-modal>
 </template>
 
 <script>
-import { message } from 'ant-design-vue';
+import { message } from "ant-design-vue";
 import { addUser } from "../service/user";
-import { encrypt } from '../utils'
+import { encrypt } from "../utils";
 
 export default {
   props: {
     visible: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
-  emits: ['update:visible', 'success'],
+  emits: ["update:visible", "success"],
   data() {
     let checkUsername = async (rule, value) => {
-      if(value === '') {
-        return Promise.resolve(); 
-      }
-      if(!/^[0-9a-zA-Z_]+$/.test(value)) {
-        return Promise.reject('只能是字母, 数字, 下划线');
-      }
-      if((value.length < 4) || (value.length > 16)) {
-        return Promise.reject('长度为4-16位');
-      }
-      return Promise.resolve(); 
-    };
-    let checkPassword = async (rule, value) => {
-      if(value === '') {
-        return Promise.resolve(); 
-      }
-      if(/(?=.*[\u0100-\uffff])/.test(value)) {
-        return Promise.reject('包含大小写英文字母、数字、特殊符号，且长度为8-20位');
-      }
-      if(!/(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[`~!@#$%^&*()-_=+{}[\]\|;:'"<>,.?/ ])/.test(value)) {
-        return Promise.reject('包含大小写英文字母、数字、特殊符号，且长度为8-20位');
-      }
-      if((value.length < 8) || (value.length > 20)) {
-        return Promise.reject('长度为8-20位');
-      }
-      return Promise.resolve(); 
-    };
-    let checkPhone = async (rule, value) => {
-      if(value === '') {
+      if (value === "") {
         return Promise.resolve();
       }
-      if(!/^[0-9]{1,13}$/.test(value)) {
-        return Promise.reject('请输入正确的手机号');
+      if (!/^[0-9a-zA-Z_]+$/.test(value)) {
+        return Promise.reject("只能是字母, 数字, 下划线");
       }
-      return Promise.resolve(); 
-    }
+      if (value.length < 4 || value.length > 16) {
+        return Promise.reject("长度为4-16位");
+      }
+      return Promise.resolve();
+    };
+    let checkPassword = async (rule, value) => {
+      if (value === "") {
+        return Promise.resolve();
+      }
+      if (/(?=.*[\u0100-\uffff])/.test(value)) {
+        return Promise.reject(
+          "包含大小写英文字母、数字、特殊符号，且长度为8-20位"
+        );
+      }
+      if (
+        !/(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[`~!@#$%^&*()-_=+{}[\]\|;:'"<>,.?/ ])/.test(
+          value
+        )
+      ) {
+        return Promise.reject(
+          "包含大小写英文字母、数字、特殊符号，且长度为8-20位"
+        );
+      }
+      if (value.length < 8 || value.length > 20) {
+        return Promise.reject("长度为8-20位");
+      }
+      return Promise.resolve();
+    };
+    let checkPhone = async (rule, value) => {
+      if (value === "") {
+        return Promise.resolve();
+      }
+      if (!/^[0-9]{1,13}$/.test(value)) {
+        return Promise.reject("请输入正确的手机号");
+      }
+      return Promise.resolve();
+    };
     return {
       loading: false,
       modalVisible: this.visible,
       modelState: {
-        username: '',
-        phone: '',
-        email: '',
-        password: ''
+        username: "",
+        phone: "",
+        email: "",
+        password: "",
       },
       rules: {
         username: [
-          { required: true, message:'请输入用户名', trigger: 'change' },
-          { validator: checkUsername, trigger: 'change' }
+          { required: true, message: "请输入用户名", trigger: "change" },
+          { validator: checkUsername, trigger: "change" },
         ],
         password: [
-          { required: true, message:'请输入初始密码', trigger: 'change' },
-          { validator: checkPassword, trigger: 'change' }
+          {
+            required: true,
+            message: "包含大小写英文字母、数字、特殊符号，且长度为8-20位",
+            trigger: "change",
+          },
+          { validator: checkPassword, trigger: "change" },
         ],
-        phone: [
-          { validator: checkPhone, trigger: 'change' }
-        ],
+        phone: [{ validator: checkPhone, trigger: "change" }],
       },
       labelCol: { span: 4 },
       wrapperCol: { span: 18 },
-    }
+    };
   },
   watch: {
     visible() {
-      this.modalVisible = this.visible
-    }
+      this.modalVisible = this.visible;
+    },
   },
   methods: {
     afterCloseHandler() {
-      this.$emit('update:visible', false)
+      this.$emit("update:visible", false);
     },
     createUser() {
-      this.$refs.model.validate().then(() => {
-        this.loading = true
-        let param = Object.assign({}, this.modelState)
-        param.password = encrypt(param.password, 'adminkeysuanpan9')
-        addUser(param).then(res => {
-          if(res.data && (res.data.code == 1)) {
-            this.$emit('success', {username: param.username, password: this.modelState.password})
-            this.modalVisible = false
-          }else {
-            throw new Error(res.data.message)
-          }
-        }).catch(err => {
-          console.error('add user error:', err)
-          message.error(`创建失败，${err.message}`)
-        }).finally(() => {
-          this.loading = false
+      this.$refs.model
+        .validate()
+        .then(() => {
+          this.loading = true;
+          let param = Object.assign({}, this.modelState);
+          param.password = encrypt(param.password, "adminkeysuanpan9");
+          addUser(param)
+            .then((res) => {
+              if (res.data && res.data.code == 1) {
+                this.$emit("success", {
+                  username: param.username,
+                  password: this.modelState.password,
+                });
+                this.modalVisible = false;
+              } else {
+                throw new Error(res.data.message);
+              }
+            })
+            .catch((err) => {
+              console.error("add user error:", err);
+              message.error(`创建失败，${err.message}`);
+            })
+            .finally(() => {
+              this.loading = false;
+            });
         })
-      }).catch(() => {})
+        .catch(() => {});
     },
     cancel() {
-      this.modalVisible = false
-    }
-  }
-}
+      this.modalVisible = false;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/components/UserUpdate.vue
+++ src/components/UserUpdate.vue
@@ -1,17 +1,19 @@
 <template>
-  <a-modal 
-    v-model:visible="modalVisible" 
+  <a-modal
+    v-model:visible="modalVisible"
     title="编辑"
     :maskClosable="false"
     :footer="null"
-    :afterClose="afterCloseHandler">
-      <a-form
-        ref="model"
-        :model="modelState"
-        :rules="rules"
-        :label-col="labelCol"
-        :wrapper-col="wrapperCol"
-      >
+    :afterClose="afterCloseHandler"
+  >
+    <a-form
+      ref="model"
+      :model="modelState"
+      :rules="rules"
+      :label-col="labelCol"
+      :wrapper-col="wrapperCol"
+      :form="form"
+    >
       <a-form-item label="用户名" name="username">
         <a-input v-model:value="modelState.username" />
       </a-form-item>
@@ -21,178 +23,225 @@
       <a-form-item label="邮箱" name="email">
         <a-input v-model:value="modelState.email" />
       </a-form-item>
-      <a-form-item label="密码" name="password" :rules="passwordRules">
-        <a-input v-if="passwordReset" style="width: 200px" v-model:value="modelState.password" autocomplete="off" />
-        <a-button 
-          :style="{marginLeft: passwordReset ? '10px' : '0'}" 
-          type="primary" 
-          @click="passwordReset=!passwordReset"
-        > 
-          {{ passwordReset ? '取消重置' : '重置' }}
+      <a-form-item label="密码" name="password" v-show="!passwordReset">
+        <a-button
+          :style="{ marginLeft: passwordReset ? '10px' : '0' }"
+          type="primary"
+          @click="resetClick()"
+        >
+          {{ passwordReset ? "取消重置" : "重置" }}
+        </a-button>
+      </a-form-item>
+      <a-form-item label="密码" name="password" v-show="passwordReset">
+        <a-input
+          style="width: 200px"
+          v-model:value="modelState.password"
+          autocomplete="off"
+        />
+        <a-button
+          :style="{ marginLeft: passwordReset ? '10px' : '0' }"
+          type="primary"
+          @click="resetClick()"
+        >
+          {{ passwordReset ? "取消重置" : "重置" }}
         </a-button>
       </a-form-item>
       <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
         <a-button type="default" @click="cancel">取消</a-button>
-        <a-button style="margin-left: 10px" type="primary" :loading="loading" @click="updateUser">确定</a-button>
+        <a-button
+          style="margin-left: 10px"
+          type="primary"
+          :loading="loading"
+          @click="updateUser"
+          >确定</a-button
+        >
       </a-form-item>
     </a-form>
   </a-modal>
 </template>
 
 <script>
-import { message } from 'ant-design-vue';
+import { message } from "ant-design-vue";
 import { updateUser, updateUserPassword } from "../service/user";
-import { encrypt } from '../utils'
+import { encrypt } from "../utils";
 
 export default {
   props: {
     visible: {
       type: Boolean,
-      default: false
+      default: false,
     },
     user: {
-      required: true
-    }
+      required: true,
+    },
   },
-  emits: ['update:visible', 'confirm'],
+  emits: ["update:visible", "confirm"],
   data() {
     let checkUsername = async (rule, value) => {
-      if(value === '') {
-        return Promise.resolve(); 
-      }
-      if(!/^[0-9a-zA-Z_]+$/.test(value)) {
-        return Promise.reject('只能是字母, 数字, 下划线');
-      }
-      if((value.length < 4) || (value.length > 16)) {
-        return Promise.reject('长度为4-16位');
-      }
-      return Promise.resolve(); 
-    };
-    let checkPhone = async (rule, value) => {
-      if(value === '') {
+      if (value === "") {
         return Promise.resolve();
       }
-      if(!/^[0-9]{1,13}$/.test(value)) {
-        return Promise.reject('请输入正确的手机号');
+      if (!/^[0-9a-zA-Z_]+$/.test(value)) {
+        return Promise.reject("只能是字母, 数字, 下划线");
       }
-      return Promise.resolve(); 
-    }
+      if (value.length < 4 || value.length > 16) {
+        return Promise.reject("长度为4-16位");
+      }
+      return Promise.resolve();
+    };
+    let checkPhone = async (rule, value) => {
+      if (value === "") {
+        return Promise.resolve();
+      }
+      if (!/^[0-9]{1,13}$/.test(value)) {
+        return Promise.reject("请输入正确的手机号");
+      }
+      return Promise.resolve();
+    };
+    let checkPassword = async (rule, value) => {
+      if (value === "") {
+        return Promise.resolve();
+      }
+      if (/(?=.*[\u0100-\uffff])/.test(value)) {
+        return Promise.reject(
+          "包含大小写英文字母、数字、特殊符号，且长度为8-20位"
+        );
+      }
+      if (
+        !/(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[`~!@#$%^&*()-_=+{}[\]\|;:'"<>,.?/ ])/.test(
+          value
+        )
+      ) {
+        return Promise.reject(
+          "包含大小写英文字母、数字、特殊符号，且长度为8-20位"
+        );
+      }
+      if (value.length < 8 || value.length > 20) {
+        return Promise.reject("长度为8-20位");
+      }
+      return Promise.resolve();
+    };
     return {
       modalVisible: this.visible,
       modelState: {
         username: this.user.username,
         phone: this.user.phone,
         email: this.user.email,
-        password: ''
+        password: "",
       },
       rules: {
         username: [
-          { required: true, message:'请输入用户名', trigger: 'change' },
-          { validator: checkUsername, trigger: 'change' }
+          { required: true, message: "请输入用户名", trigger: "change" },
+          { validator: checkUsername, trigger: "change" },
         ],
-        phone: [
-          { validator: checkPhone, trigger: 'change' }
+        phone: [{ validator: checkPhone, trigger: "change" }],
+        password: [
+          {
+            required: true,
+            message: "包含大小写英文字母、数字、特殊符号，且长度为8-20位",
+            trigger: "change",
+          },
+          { validator: checkPassword, trigger: "change" },
         ],
       },
       labelCol: { span: 4 },
       wrapperCol: { span: 18 },
       passwordReset: false,
-      loading: false
-    }
-  },
-  computed: {
-    passwordRules() {
-      let checkPassword = async (rule, value) => {
-      if(value === '') {
-        return Promise.resolve(); 
-      }
-      if(/(?=.*[\u0100-\uffff])/.test(value)) {
-        return Promise.reject('包含大小写英文字母、数字、特殊符号，且长度为8-20位');
-      }
-      if(!/(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[`~!@#$%^&*()-_=+{}[\]\|;:'"<>,.?/ ])/.test(value)) {
-        return Promise.reject('包含大小写英文字母、数字、特殊符号，且长度为8-20位');
-      }
-      if((value.length < 8) || (value.length > 20)) {
-        return Promise.reject('长度为8-20位');
-      }
-      return Promise.resolve(); 
+      loading: false,
     };
-      return this.passwordReset ? [
-          { required: true, message:'请输入初始密码', trigger: 'change' },
-          { validator: checkPassword, trigger: 'change' }
-        ] : []
-    }
   },
+  computed: {},
   watch: {
     visible() {
-      this.modalVisible = this.visible
-    }
+      this.modalVisible = this.visible;
+    },
   },
   methods: {
     afterCloseHandler() {
-      this.$emit('update:visible', false)
+      this.$emit("update:visible", false);
     },
     cancel() {
-      this.modalVisible = false
+      this.modalVisible = false;
     },
     updateUser() {
-      this.$refs.model.validate().then(() => {
-        this.loading = true
-        let userParam = {
-          id: this.user.id,
-          username: this.modelState.username,
-          phone: this.modelState.phone,
-          email: this.modelState.email
-        }
+      this.$refs.model
+        .validate()
+        .then(() => {
+          this.loading = true;
+          let userParam = {
+            id: this.user.id,
+            username: this.modelState.username,
+            phone: this.modelState.phone,
+            email: this.modelState.email,
+          };
 
-        if(!this.passwordReset) {
-          updateUser(userParam).then(res => {
-            if(res.data.code == 1) {
-              this.$emit('success', false)
-            }else {
-              throw new Error(res.data.message)
-            }
-          }).catch(err => {
-            console.error('update user error:', err)
-            message.error(`修改失败，${err.message}`)
-          }).finally(() => {
-            this.loading = false
-          })
-        }else {
-          let passwordParam = {
-            userId: this.user.id,
-            password: encrypt(this.modelState.password, 'adminkeysuanpan9')
+          if (!this.passwordReset) {
+            updateUser(userParam)
+              .then((res) => {
+                if (res.data.code == 1) {
+                  this.$emit("success", false);
+                } else {
+                  throw new Error(res.data.message);
+                }
+              })
+              .catch((err) => {
+                console.error("update user error:", err);
+                message.error(`修改失败，${err.message}`);
+              })
+              .finally(() => {
+                this.loading = false;
+              });
+          } else {
+            let passwordParam = {
+              userId: this.user.id,
+              password: encrypt(this.modelState.password, "adminkeysuanpan9"),
+            };
+            Promise.all([
+              updateUser(userParam),
+              updateUserPassword(passwordParam),
+            ])
+              .then((res) => {
+                if (res[0].data.code == 1 && res[1].data.code == 1) {
+                  this.$emit("success", true, {
+                    username: this.modelState.username,
+                    password: this.modelState.password,
+                  });
+                  this.modalVisible = false;
+                } else {
+                  let errorMsgs = [];
+                  if (res[0].data.code != "1") {
+                    errorMsgs.push(res[0].data.message);
+                  }
+                  if (res[1].data.code != "1") {
+                    errorMsgs.push(res[1].data.message);
+                  }
+                  throw new Error(errorMsgs.join(","));
+                }
+              })
+              .catch((err) => {
+                console.error("update user error:", err);
+                message.error(`修改失败，${err.message}`);
+              })
+              .finally(() => {
+                this.loading = false;
+              });
           }
-          Promise.all([
-            updateUser(userParam),
-            updateUserPassword(passwordParam)
-          ]).then(res => {
-            if((res[0].data.code == 1) && (res[1].data.code == 1)) {
-              this.$emit('success', true, {username: this.modelState.username, password: this.modelState.password})
-              this.modalVisible = false
-            }else {
-              let errorMsgs = []
-              if(res[0].data.code != '1') {
-                errorMsgs.push(res[0].data.message)
-              }
-              if(res[1].data.code != '1') {
-                errorMsgs.push(res[1].data.message)
-              }
-              throw new Error(errorMsgs.join(','))
-            }
-          }).catch(err => {
-            console.error('update user error:', err)
-            message.error(`修改失败，${err.message}`)
-          }).finally(() => {
-            this.loading = false
-          })
-        }
-      }).catch(() => {})
-    }
-  }
-}
+        })
+        .catch(() => {});
+    },
+    resetClick() {
+      if (this.passwordReset === true) {
+        const newValue = {
+          ...this.modelState,
+          password: "",
+        };
+        this.modelState = newValue;
+      }
+      this.passwordReset = !this.passwordReset;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
